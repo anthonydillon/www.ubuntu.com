0436e2ca878eaa4a724f696a3941e15210e52797
Anthony Dillon <me@anthonydillon.com>
2020-09-17 11:59:41 +0100

    Bump cookie policy to 3.0.0

--- static/js/src/core.js
+++ static/js/src/core.js
@@ -5,9 +5,4 @@ import { cookiePolicy } from "@canonical/cookie-policy";
 createNav({ showLogins: false });
 
 // Initalise the cookie policy notification.
-
-var options = {
-  content: `We use cookies to improve your experience. By your continued use of this site you accept such use.<br /> This notice will disappear by itself.`,
-  duration: 10000,
-};
-cookiePolicy(options);
+cookiePolicy();
